838689fbda9faa61b3fe7413c11f78ecb586a6db
Zach Gatu <zkgatu@gmail.com>
2021-10-02 17:59:05 +0100

    Made a change

--- src/main/java/me/zach/demo/sum/SumService.java
+++ src/main/java/me/zach/demo/sum/SumService.java
@@ -1,14 +1,8 @@
 package me.zach.demo.sum;
 
 public class SumService {
-    private DataService dataService;
-
-    public SumService(DataService dataService) {
-        this.dataService = dataService;
-    }
 
     public int sum(int a, int b) {
-        dataService.getSavedNumbers();
-        return a + b;
+        return a - b;
     }
 }
--- src/test/java/me/zach/demo/sum/SumImplmentationTest.java
+++ src/test/java/me/zach/demo/sum/SumImplmentationTest.java
@@ -1,19 +1,13 @@
 package me.zach.demo.sum;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 public class SumImplmentationTest {
-    @Mock
-    private DataService dataService;
-
     @Test
     public void when_inputA_and_inputB_Are_Added_then_ResultShouldBe_Sum_of_A_and_B() {
-        when(dataService.getSavedNumbers()).thenReturn(2);
-        SumService sumService = new SumService(dataService);
+        SumService sumService = new SumService();
         int a = 1;
         int b = 1;
         int expectedResult = 2;
